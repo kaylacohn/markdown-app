f55e13b4973bda7787af800e5e14acaa5214ba0b
Kayla Cohn <kayla.cohn@salesloft.com>
2017-05-09 08:08:54 -0400

    Initial setup

---
+++ app/renderer.js
@@ -0,0 +1,9 @@
+const marked = require('marked');
+
+const markdownView = document.querySelector('#markdown');
+const htmlVIew = document.querySelector('#html');
+const newFileButton = document.querySelector('#new-file');
+const openFileButton = document.querySelector('#save-markdown');
+const saveMarkdownButton = document.querySelector('#save-markdown');
+const revertButton = document.querySelector('#revert');
+const saveHtmlButton = document.querySelector('#save-html');
